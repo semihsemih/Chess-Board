6125cf8bf6cd2c2f8cf11a33f4ebb00101226757
Semih Arslan <semihsemih@me.com>
2020-03-29 17:07:57 +0300

    final commit

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,33 @@ namespace ChessBoard
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size =new Size(400, 400);
+
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50;
+                    buttons[i,j].BackColor = (i+j)%2 == 0 ? Color.Black : Color.White;
+                    
+                    this.Controls.Add(buttons[i, j]);
+                }
+
+                top += 50;
+                left = 0;
+            }
+        }
     }
 }
